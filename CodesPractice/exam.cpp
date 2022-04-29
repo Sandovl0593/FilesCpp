b2a1388150a0f05ff5c6c2116377ebabcb698969
SandovalAdrianUTEC <adrian.sandoval@utec.edu.pe>
2022-04-29 16:59:17 -0500

    newAdd

--- CodesPractice/exam.cpp
+++ CodesPractice/exam.cpp
@@ -1,66 +1,37 @@
 #include <iostream>
-using namespace std;
-
-// int main() {
-//     srand(time(nullptr));
-//     const int M = 10, N = 10;
-//     int matrix[M][N];
-
-//     for (int i=0; i<M;i++) {
-//         for (int j=0; i<N;j++) {
-//             matrix[i][j] = rand()%2;
-//         }
-//     }
-//     for (int i=0; i<M;i++) {
-//         for (int j=0; i<N;j++) {
-//             cout << matrix[i][j] << endl;
-//         }
-//         cout << endl;
-//     }
-// }
-#include <iostream>
-#include <string>
 #include <sstream>
-#include <vector>
+#include <string>
+#include "identpowertype.h"
+
 using namespace std;
 
-bool isVocal(char voc) {
-    for (char x: "aeiou") {
-        if (x == voc)       return true;
-    }  return false;
+int CalcularValor(string* listCartas, int cant) {
+    int valortot = 0;
+    for (int i=0; i<cant; i++) {
+        int valor;
+        string* conj = SepararTypes(listCartas[i]); 
+        char type = (*(conj+1))[0];
+        valor = getPoder(*(conj+0));
+        valor *= getvalor(type);
+        valortot += valor;
+        delete[] conj;
+    }
+    return valortot;
 }
 
 int main() {
-    char dato[40];  cout << "Imgresa: ";
-    cin.getline(dato, 40);
-    // distribuir objetos en una lista
-    string *letters = new string[20];
-    istringstream ss(dato);
-    string del;   int i = 0;
-    while (getline(ss, del, ' ')) {
-        letters[i] = del;    i++;
-    }
-    // defnum : el primer elemento como int
-    stringstream num;
-    string num_ = letters[0];   int defnum;
-    num << num_;    num >> defnum;
+    int cant;
+    do { cout << "Ingrese cantidad de cartas: ";  cin >> cant;
+    } while (cant < 1 && cant > 6);
 
-    if (defnum == sizeof(letters+1)-1) {
-        int c = 0;
-        for (auto palabra: letters) {
-            int vocales = 0;   i = 0;
-            char letras[] = {};
-            for (char letra: palabra) {
-                bool valid = false;
-                for (char let: letras)
-                    if (let != letra) {
-                        letras[i] = letra;
-                        valid = true;  i++;    break;
-                    }
-                if (isVocal(letra) || valid)    vocales++;
-            }
-            if (vocales >= 3)     c++;
-        }
-        cout << "Resultado: " << c << endl;
-    }
+    char conjCartas[cant*3];    
+    cout << "Ingrese cartas: ";     cin.getline(conjCartas, 18);
+    istringstream ss(conjCartas);   string div; 
+    string* listCartas;
+
+    int i=0;
+    while (getline(ss, div, ' '))
+        listCartas[i] = div;    i++;
+    int valortot = CalcularValor(listCartas, cant);
+    cout << "Valor de cartas: " << valortot << endl;
 }
--- CodesPractice/identpowertype.h
+++ CodesPractice/identpowertype.h
@@ -1,8 +1,43 @@
-#ifndef _IDENTPOWERTYPE
-#define _IDENTPOWERTYPE
+#pragma once
+#ifndef IDENTPOWERTYPE
+#define IDENTPOWERTYPE
 
-int PowerType(string carta) {
+using namespace std;
 
+int getPoder(string let) {
+    string lets[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    const int valuesLet[] = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+
+    int valor;
+    for (int i=0; i<13;i++) {
+        if (lets[i] == let)
+            valor = valuesLet[i];
+    }
+    return valor;
 }
 
-# endif 
+int getvalor(char type) {
+    int valor;
+    switch (type) {
+        case 'S':   valor = 4;    break;
+        case 'H':   valor = 3;    break;
+        case 'D':   valor = 2;    break;
+        case 'C':   valor = 1;    break;
+        default: break;
+    }
+    return valor;
+}
+
+string* SepararTypes(string carta) {
+    string* lisChar = new string[2];
+    if (size(carta) == 3) {
+        lisChar[0] = carta.substr(0, 2);
+        lisChar[1] = carta.substr(2, 1);
+    } else {
+        lisChar[0] = carta.substr(0, 1);
+        lisChar[1] = carta.substr(1, 1); 
+    }
+    return lisChar;
+}
+
+# endif // FILE: ../_IDENTPOWERTYPE.H
